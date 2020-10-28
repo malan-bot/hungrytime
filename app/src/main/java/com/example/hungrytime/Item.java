79a786e9c42015a2969b861de58a779b6d24beeb
JuwonHan <rrd0649@autuni.ac.nz>
2020-10-28 18:12:39 +1300

    Create Item.java

---
+++ app/src/main/java/com/example/hungrytime/Item.java
@@ -0,0 +1,58 @@
+package com.example.hungrytime;
+
+import java.util.ArrayList;
+
+public class Item {
+    private String item;
+    private ArrayList<String> recipes;
+    private String quantity;
+    private boolean expanded;
+
+    public Item(String item, ArrayList<Recipe> recipes, String quantity, boolean expanded) {
+        this.item = item;
+        this.quantity = quantity;
+        this.expanded = false;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public ArrayList<String> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(ArrayList<String> recipes) {
+        this.recipes = recipes;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isExpanded() {
+        return expanded;
+    }
+
+    public void setExpanded(boolean expanded) {
+        this.expanded = expanded;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "item='" + item + '\'' +
+                ", recipes=" + recipes.toString() + '\'' +
+                ", quantity='" + quantity + '\'' +
+                ", expanded=" + expanded +
+                '}';
+    }
+}
